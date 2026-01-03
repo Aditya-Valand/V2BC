67626fa0cf3a46a538018ad0798b49c891a94c58
Anirudh Singh <violentanirudh@gmail.com>
2026-01-03 21:08:44 +0530

    Users | Profile UI

--- web/routes/dashboard.py
+++ web/routes/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from database.user import get_user_by_email
 from middlewares import token_required
 
@@ -7,4 +7,12 @@ dashboard_bp = Blueprint('dashboard', __name__)
 @dashboard_bp.route('/dashboard', methods=['GET'])
 @token_required
 def dashboard(user):
+    if user['profile'] == 0:
+        return redirect(url_for('dashboard.profile'))
     return render_template('dashboard.html', user=user)
+
+
+@dashboard_bp.route('/profile', methods=['GET'])
+@token_required
+def profile(user):
+    return render_template('profile.html', user=user)
